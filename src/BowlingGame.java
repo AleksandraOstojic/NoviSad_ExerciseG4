e826c94b4c1d5ed5a0c8259872e73080842ff63c
somename <someemail>
2017-06-09 20:47:48 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -28,17 +28,21 @@ public class BowlingGame {
 	public void setBonus(int firstThrow, int secondThrow) {
 		for(int i=0; i<frames.size(); i++)
 		{
-			
-			if(frames.get(i).isStrike())
+			if(frames.get(frames.size()-1).isStrike())
+			{
+				bonus = new Frame(firstThrow, secondThrow);
+				frames.add(bonus);
+			}
+			else if(frames.get(i).isStrike())
 			{	bonus = new Frame(frames.get(i+1).getFirstThrow(), frames.get(i+1).getSecondThrow());
 				frames.add(bonus);
 			}
 			//slucaj kada je postgnut spare, ili ako se radi o poslednjem frame-u, ostvaruje se pravo na jedno dodatno bacanje
-			else if(frames.get(i).isSpare() || frames.get(frames.size()-1).isSpare() || frames.get(frames.size()-1).isStrike())
+			else if(frames.get(i).isSpare() )
 			{
 				bonus = new Frame(frames.get(i+1).getFirstThrow(), 0);
 			}
-			
+			//||frames.get(frames.size()-1).isSpare() || frames.get(frames.size()-1).isStrike()
 		}
 	}
 	
