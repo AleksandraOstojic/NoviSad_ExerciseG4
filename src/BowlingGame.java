a3c2f8dfe14cf8fa0aac025173489f38edc73aa8
somename <someemail>
2017-06-09 20:58:46 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -26,25 +26,14 @@ public class BowlingGame {
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
-		for(int i=0; i<frames.size(); i++)
-		{
-			
-			if(frames.get(i).isStrike())
-			{	bonus = new Frame(frames.get(i+1).getFirstThrow(), frames.get(i+1).getSecondThrow());
-				frames.add(bonus);
-			}
-			else if(frames.get(i).isSpare() )
-			{
-				bonus = new Frame(frames.get(i+1).getFirstThrow(), 0);
-			}
 			//poslednji frame
-			else if(frames.get(frames.size()-1).isSpare() || frames.get(frames.size()-1).isStrike())
+		if(frames.get(frames.size()-1).isSpare() || frames.get(frames.size()-1).isStrike())
 			{
 				bonus = new Frame(firstThrow, 0);
 			}
 			
 		}
-	}
+	
 	
 	// Returns the game score
 	public int score() throws BowlingException{
