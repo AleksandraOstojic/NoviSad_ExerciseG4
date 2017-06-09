24eb6ebb2908ef91d1591ceb9bd17f805d59edb8
somename <someemail>
2017-06-09 20:04:08 +0200

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -2,9 +2,16 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	
-	public Frame(int firstThrow, int secondThrow){
-		this.firstThrow = firstThrow;
-		this.secondThrow = secondThrow;
+	public Frame(int firstThrow, int secondThrow) throws BowlingException{
+		if(firstThrow <=10 && firstThrow + secondThrow <=10)
+		{
+			this.firstThrow = firstThrow;
+			this.secondThrow = secondThrow;
+		}
+		else
+		{
+			throw new BowlingException();
+		}
 	}
 	
 	public int getFirstThrow() {
