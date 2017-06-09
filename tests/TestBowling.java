da89b172c3828976817fae48b3d0a212124f419d
somename <someemail>
2017-06-09 20:07:43 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -10,7 +10,7 @@ public class TestBowling {
 	}*/
 	
 	@Test
-	public void test_initializaton()
+	public void test_initializaton() throws BowlingException
 	{
 		Frame frame = new Frame(3,4);
 		assertNotNull(frame);
@@ -26,6 +26,17 @@ public class TestBowling {
 	public void test_invalid_number_of_pins2() throws BowlingException
 	{
 		Frame frame = new Frame(2,10);
+	}
+	
+	@Test
+	public void test_strike_method() throws BowlingException{
+		Frame frame = new Frame(10,0);
+		assertTrue(frame.isStrike());
+	}
+	@Test
+	public void test_spare_method() throws BowlingException{
+		Frame frame = new Frame(6,4);
+		assertTrue(frame.isSpare());
 	}
 
 }
